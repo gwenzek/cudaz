55526973cc30b4e9d77e88f9b72a698c93047062
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2021-11-22 18:15:29 +0100

    transposePerCell

--- CS344/src/lesson5.zig
+++ CS344/src/lesson5.zig
@@ -1,9 +1,13 @@
+//! Compare implementations of different transpose algorithms
+//! Compile with Zig, then run: sudo /usr/local/cuda/bin/ncu ./CS344/zig-out/bin/lesson5 > ./CS344/resources/lesson5/ncu_report.txt
+//! This will generate
 const std = @import("std");
 const Random = std.rand.DefaultPrng;
 const log = std.log.scoped(.lesson5);
 
 const cuda = @import("cudaz");
-const kernels = @import("lesson5_kernel.zig");
+const cu = cuda.cu;
+const RawKernels = @import("lesson5_kernel.zig");
 
 pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -18,23 +22,26 @@ pub fn main() !void {
     defer allocator.free(trans_cpu);
 
     var timer = std.time.Timer.start() catch unreachable;
-    kernels.transposeCpu(data, trans_cpu, num_cols);
+    RawKernels.transposeCpu(data, trans_cpu, num_cols);
     const elapsed_cpu = @intToFloat(f32, timer.lap()) / std.time.ns_per_us;
 
     log.info("CPU transpose of {}x{} matrix took {:.3}ms", .{ num_cols, num_cols, elapsed_cpu });
-    var stream = try cuda.Stream.init(0);
-
+    var stream = initStreamAndModule(0);
     const d_data = try cuda.allocAndCopy(u32, data);
     const d_trans = try cuda.alloc(u32, data.len);
-    const elapsed_gpu = try gpuTransposeSerial(&stream, d_data, d_trans, num_cols);
-
+    var elapsed_gpu = try transposeSerial(&stream, d_data, d_trans, num_cols);
     log.info("GPU serial transpose of {}x{} matrix took {:.3}ms", .{ num_cols, num_cols, elapsed_gpu });
+
+    elapsed_gpu = try transposePerRow(&stream, d_data, d_trans, num_cols);
+    log.info("GPU transpose per row of {}x{} matrix took {:.3}ms", .{ num_cols, num_cols, elapsed_gpu });
+
+    elapsed_gpu = try transposePerCell(&stream, d_data, d_trans, num_cols);
+    log.info("GPU transpose per cell of {}x{} matrix took {:.3}ms", .{ num_cols, num_cols, elapsed_gpu });
 }
 
-fn gpuTransposeSerial(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, num_cols: usize) !f64 {
-    const transposeCpu = try cuda.FnStruct("transposeCpu", kernels.transposeCpu).init();
+fn transposeSerial(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, num_cols: usize) !f64 {
     var timer = cuda.GpuTimer.start(stream);
-    try transposeCpu.launch(
+    try k.transposeCpu.launch(
         stream,
         cuda.Grid.init1D(1, 1),
         .{ d_data, d_out, num_cols },
@@ -43,14 +50,52 @@ fn gpuTransposeSerial(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, n
     return timer.elapsed();
 }
 
-fn gpu_transpose_one_row(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, num_cols: usize) !f64 {
-    const transpose = try cuda.FnStruct("transpose_one_row", kernels.transpose_one_row).init();
+fn transposePerRow(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, num_cols: usize) !f64 {
     var timer = cuda.GpuTimer.start(stream);
-    try transpose.launch(
+    try k.transposePerRow.launch(
         stream,
-        cuda.Grid.init1D(1, 1),
+        cuda.Grid.init1D(num_cols, 32),
         .{ d_data, d_out, num_cols },
     );
     timer.stop();
     return timer.elapsed();
+}
+
+fn transposePerCell(stream: *cuda.Stream, d_data: []const u32, d_out: []u32, num_cols: usize) !f64 {
+    var timer = cuda.GpuTimer.start(stream);
+    try k.transposePerCell.launch(
+        stream,
+        cuda.Grid.init2D(num_cols, num_cols, 32, 32),
+        .{ d_data, d_out, num_cols },
+    );
+    timer.stop();
+    return timer.elapsed();
+}
+
+const Kernels = struct {
+    transposeCpu: cuda.FnStruct("transposeCpu", RawKernels.transposeCpu),
+    transposePerRow: cuda.FnStruct("transposePerRow", RawKernels.transposePerRow),
+    transposePerCell: cuda.FnStruct("transposePerCell", RawKernels.transposePerCell),
+};
+var k: Kernels = undefined;
+
+fn initStreamAndModule(device: u8) cuda.Stream {
+    const stream = cuda.Stream.init(device) catch unreachable;
+    // Panic if we can't load the module.
+    k = Kernels{
+        .transposeCpu = @TypeOf(k.transposeCpu).init() catch unreachable,
+        .transposePerRow = @TypeOf(k.transposePerRow).init() catch unreachable,
+        .transposePerCell = @TypeOf(k.transposePerCell).init() catch unreachable,
+    };
+    return stream;
+}
+
+fn peekBandwith(device: u8) f32 {
+    var d: cu.CUdevice = null;
+    cu.check(cu.cuDeviceGetProperties(&d, device)) catch unreachable;
+    var props: cu.CUdevprop = undefined;
+    cu.check(cu.cuDeviceGetProperties(&props, d)) catch unreachable;
+    const mem_clock_rate_hz = props.memoryClockRate * 1e3;
+    const mem_bus_width_bytes = @divExact(props.memoryBusWidth, 8);
+    return mem_clock_rate_hz * mem_bus_width_bytes;
 }
--- CS344/src/lesson5_kernel.zig
+++ CS344/src/lesson5_kernel.zig
@@ -18,6 +18,12 @@ pub export fn transposePerRow(data: []const u32, trans: []u32, num_cols: usize) 
     }
 }
 
+pub export fn transposePerCell(data: []const u32, trans: []u32, num_cols: usize) callconv(.PtxKernel) void {
+    const i = getIdX();
+    const j = getIdY();
+    trans[num_cols * i + j] = data[num_cols * j + i];
+}
+
 const builtin = @import("builtin");
 const is_nvptx = builtin.cpu.arch == .nvptx64;
 
@@ -45,6 +51,33 @@ pub inline fn gridIdX() usize {
     return @intCast(usize, ctaid);
 }
 
+pub inline fn threadIdY() usize {
+    if (!is_nvptx) return 0;
+    var tid = asm volatile ("mov.u32 \t$0, %tid.y;"
+        : [ret] "=r" (-> u32),
+    );
+    return @intCast(usize, tid);
+}
+
+pub inline fn threadDimY() usize {
+    if (!is_nvptx) return 0;
+    var ntid = asm volatile ("mov.u32 \t$0, %ntid.y;"
+        : [ret] "=r" (-> u32),
+    );
+    return @intCast(usize, ntid);
+}
+
+pub inline fn gridIdY() usize {
+    if (!is_nvptx) return 0;
+    var ctaid = asm volatile ("mov.u32 \t$0, %ctaid.y;"
+        : [ret] "=r" (-> u32),
+    );
+    return @intCast(usize, ctaid);
+}
+
 pub inline fn getIdX() usize {
     return threadIdX() + threadDimX() * gridIdX();
+}
+pub inline fn getIdY() usize {
+    return threadIdY() + threadDimY() * gridIdY();
 }
